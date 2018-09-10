dab9f6c14e9f026a7da2e11be7049c091ed88117
Calvin Mann <calvin_m@hotmail.com>
2018-09-10 12:00:48 -0700

    Added a new challege to translate english into pig latin

---
+++ PigLatinTranslation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatinTranslation
+{
+    class Program
+    {
+        //Translate english word to pig latin
+        //instructions are here 
+
+        readonly char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+        static void Main(string[] args)
+        {
+
+
+          
+        }
+
+        public string TranslateWord(string word)
+        {
+            char[] charsInWord = word.ToCharArray();
+
+            for(int i = 0; i < charsInWord.Length - 1; i = 0)
+            {
+                if(vowels.Contains(charsInWord[i]) )
+                {
+                    if(i == 0)
+                    {
+                        //add yay and return
+                        return word + "yay";
+                    }
+                    else
+                    {
+                        //convert to string and add ay
+
+                        return "" + "ay";
+                    }
+                }
+                else
+                {
+                    // move to the end of the array
+                }
+            }
+
+            return "";
+        }
+
+        public string TranslateSentence(string sentence)
+        {
+            return "";
+        }
+    }
+}
